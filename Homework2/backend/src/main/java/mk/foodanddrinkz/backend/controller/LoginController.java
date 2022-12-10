71d9b3c61244aec2bbc3c0b0100d31c4998f3807
andrejtodorovski <82031894+andrejtodorovski@users.noreply.github.com>
2022-12-10 01:46:10 +0100

    Working on Login

--- Homework2/backend/src/main/java/mk/foodanddrinkz/backend/controller/LoginController.java
+++ Homework2/backend/src/main/java/mk/foodanddrinkz/backend/controller/LoginController.java
@@ -6,40 +6,36 @@ import mk.foodanddrinkz.backend.model.User;
 import mk.foodanddrinkz.backend.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-// Copied from webLab
-//@Controller
-//@RequestMapping("/login")
-//public class LoginController {
-//    private final UserService userService;
-//
-//    public LoginController(UserService userService) {
-//        this.userService = userService;
-//    }
-//
-//    @GetMapping
-//    public String showLoginPage(){
-//        return "login";
-//    }
-//    @PostMapping
-//    public String loginUser(@RequestParam String username,
-//                            @RequestParam String password,
-//                            Model model , HttpServletRequest request){
-//        User u;
-//        try{
-//            u=userService.login(username,password);
-//            request.getSession().setAttribute("username",username);
-//            request.getSession().setAttribute("user",u);
-//            return "redirect:/home";
-//        }
-//        catch(UserDoesntExistException exception){
-//            model.addAttribute("hasError", true);
-//            model.addAttribute("error", exception.getMessage());
-//            return "login";
-//        }
-//    }
-//}
+//Copied from webLab
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/login")
+public class LoginController {
+    private final UserService userService;
+
+    public LoginController(UserService userService) {
+        this.userService = userService;
+    }
+    @PostMapping
+    public void loginUser(@RequestBody String credentials,
+                            HttpServletRequest request){
+        credentials = credentials.substring(1,credentials.length()-1);
+        String username = credentials.split(",")[0].split(":")[1];
+        String password = credentials.split(",")[1].split(":")[1];
+        username = username.substring(1,username.length()-1);
+        password = password.substring(1,password.length()-1);
+        System.out.println(username);
+        System.out.println(password);
+        User u;
+        try{
+            u=userService.login(username,password);
+            request.getSession().setAttribute("username",username);
+            request.getSession().setAttribute("user",u);
+        }
+        catch(UserDoesntExistException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+}
--- Homework2/frontend/src/components/LoginComponent.jsx
+++ Homework2/frontend/src/components/LoginComponent.jsx
@@ -1,11 +1,110 @@
+// import React from 'react';
+// import { useNavigate, useHistory } from 'react-router';
+// import { useState } from 'react'
+
+// function LoginComponent() {
+    
+//   const [username, setUsername] = useState('');
+//   const [password, setPassword] = useState('');
+  
+//   const navigate = useNavigate();
+
+//   const handleUsername = (event) => {
+//     setUsername(event.username);
+//   };
+//   const handlePassword = (event) => {
+//     setPassword(event.password);
+//   };
+
+//   const handleLogin=(e)=>{
+//       e.preventDefault()
+//       const loc = {username,password}
+//       console.log(loc)
+//       fetch("http://localhost:8080/login/",{
+//         mode:'no-cors',
+//         method:"POST",
+//         headers:{"Content-Type":"application/json"},
+//         body:JSON.stringify(loc)
+//       }).then(()=>{
+//         navigate('/');
+//       })
+//   }
+
+//   return (
+//         <div className='container'>
+//             <div className="Auth-form-container">
+//               <form className="Auth-form">
+//                 <div className="Auth-form-content">
+//                   <h3 className="Auth-form-title">Sign In</h3>
+//                   <div className="form-group mt-3">
+//                     <label>Username</label>
+//                     <input
+//                       type="username"
+//                       className="form-control mt-1"
+//                       placeholder="Enter username"
+//                       value={username}
+//                       onChange={handleUsername}
+//                     />
+//                   </div>
+//                   <div className="form-group mt-3">
+//                     <label>Password</label>
+//                     <input
+//                       type="password"
+//                       className="form-control mt-1"
+//                       placeholder="Enter password"
+//                       value={password}
+//                       onChange={handlePassword}
+//                     />
+//                   </div>
+//                   <div className="d-grid gap-2 mt-3">
+//                     <input type="submit" value={"Log in"} onClick={handleLogin}/>
+//                   </div>
+//                 </div>
+//               </form>
+//             </div>
+//             </div>
+//           );
+// }
+
+// export default LoginComponent;
+
 import React, { Component } from 'react';
 
 class LoginComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username:'',
+            password:''
+        }
+        this.handleUsername = this.handleUsername.bind(this);
+        this.handlePassword = this.handlePassword.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleUsername = (event) => {
+        this.setState({username:event.target.value});
+    }
+    handlePassword = (event) => {
+        this.setState({password:event.target.value});
+    }
+    handleSubmit = (event) => {
+        event.preventDefault();
+        let cred = {
+            username:this.state.username,
+            password:this.state.password
+        }
+        console.log(cred)
+        fetch("http://localhost:8080/login",{
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify(cred) 
+    })
+    }
     render() {
         return (
-        <div className='container'>
+            <div className='container'>
             <div className="Auth-form-container">
-              <form className="Auth-form">
+              <form className="Auth-form" onSubmit={this.handleSubmit}>
                 <div className="Auth-form-content">
                   <h3 className="Auth-form-title">Sign In</h3>
                   <div className="form-group mt-3">
@@ -14,6 +113,8 @@ class LoginComponent extends Component {
                       type="username"
                       className="form-control mt-1"
                       placeholder="Enter username"
+                      value={this.state.username}
+                      onChange={this.handleUsername}
                     />
                   </div>
                   <div className="form-group mt-3">
@@ -22,18 +123,18 @@ class LoginComponent extends Component {
                       type="password"
                       className="form-control mt-1"
                       placeholder="Enter password"
+                      value={this.state.password}
+                      onChange={this.handlePassword}
                     />
                   </div>
                   <div className="d-grid gap-2 mt-3">
-                    <button type="submit" className="btn btn-primary">
-                      Login
-                    </button>
+                    <input type="submit" value="Log in"/>
                   </div>
                 </div>
               </form>
             </div>
             </div>
-          );
+        );
     }
 }
 
