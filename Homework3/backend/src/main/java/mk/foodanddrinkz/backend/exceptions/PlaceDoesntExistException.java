9c135bcc9739fd789c270cc4e12ae7a4f62628c2
andrejtodorovski <82031894+andrejtodorovski@users.noreply.github.com>
2023-02-05 15:59:44 +0100

    New folder creation

--- Homework3/backend/src/main/java/mk/foodanddrinkz/backend/exceptions/PlaceDoesntExistException.java
+++
@@ -1,7 +0,0 @@
-package mk.foodanddrinkz.backend.exceptions;
-
-public class PlaceDoesntExistException extends RuntimeException{
-    public PlaceDoesntExistException() {
-        super("Place doesn't exist");
-    }
-}
--- Homework3/backend/src/main/java/mk/foodanddrinkz/backend/exceptions/UserDoesntExistException.java
+++
@@ -1,7 +0,0 @@
-package mk.foodanddrinkz.backend.exceptions;
-
-public class UserDoesntExistException extends Exception{
-    public UserDoesntExistException() {
-        super("User doesn't exist");
-    }
-}
