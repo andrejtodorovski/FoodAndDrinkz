0194a223990d6971cc630ed12afa648318573922
andrejtodorovski <82031894+andrejtodorovski@users.noreply.github.com>
2023-02-07 19:11:52 +0100

    t

---
+++ Homework4/microservice2/src/test/java/com/example/microservice1/Microservice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Microservice1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
