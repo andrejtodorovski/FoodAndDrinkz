267c12c70e8ba6a8b6932cf8be2d8bf66ecfd8a5
andrejtodorovski <82031894+andrejtodorovski@users.noreply.github.com>
2023-02-11 21:25:12 +0100

    Docker and deployment

---
+++ Homework4/deploy.sh
@@ -0,0 +1,7 @@
+cd backend && ./mvnw clean package
+cd ..
+cd microservice1 && ./mvnw clean package
+cd ..
+cd microservice2 && ./mvnw clean package
+cd ..
+cd microservice3 && ./mvnw clean package
--- Homework4/frontend/src/index.js
+++ Homework4/frontend/src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDwt4p1gTgdNBweLvOiq5TP-47LgM4BQWs",
+  authDomain: "dians-8f4c7.firebaseapp.com",
+  projectId: "dians-8f4c7",
+  storageBucket: "dians-8f4c7.appspot.com",
+  messagingSenderId: "272523508341",
+  appId: "1:272523508341:web:789f03af0bf13846d3563c",
+  measurementId: "G-S45BTQQ8PF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
